b6d2f12ec501872c697b71ab1b1c38e2d31fc3fa
Checho <sergio.avila.astorga@gmail.com>
2022-01-29 19:43:26 -0300

    add add navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <v-app-bar dark>
+      <v-app-bar-nav-icon
+        @click="drawer = true"
+        class="hidden-sm-and-up"
+      ></v-app-bar-nav-icon>
+
+      <v-toolbar-title>mesapp</v-toolbar-title>
+
+      <v-toolbar-items class="ml-5 hidden-sm-and-down">
+        <v-btn to="#" plain elevation="0">Generar código QR</v-btn>
+        <v-btn to="#" plain elevation="0">Usuarios</v-btn>
+        <v-btn to="#" plain elevation="0">Menú</v-btn>
+      </v-toolbar-items>
+
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn to="#" plain elevation="0">
+            Salir <v-icon right dark> mdi-logout </v-icon>
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="grey-darken-3--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Generar código QR</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Usuarios</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Menú</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
+</script>
+
+<style scoped>
+.theme--dark.v-app-bar.v-toolbar.v-sheet{
+    background: #31302E;
+}
+.v-btn.v-size--default{
+    text-transform: initial !important;
+    font-weight: 300;
+}
+</style>
